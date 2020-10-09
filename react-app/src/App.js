94acdd4c90346757150cb8df68880b45285f3c82
lewiswolf <lewiswolstanholme@gmail.com>
2020-10-09 11:57:39 +0100

    added google analytics without an active GA key

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+// import Analytics from 'react-ga';
 import axios from 'axios'
 import './index.scss';
 
 export default function App() {
+
+  React.useEffect(() => {
+    const key = 'need to set an active key!!'
+    Analytics.initialize(key);
+    Analytics.pageview(window.location.pathname);
+  });
+
   return (
     <main>
       <button onClick={() => {
