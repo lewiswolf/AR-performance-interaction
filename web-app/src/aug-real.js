74105645a80a0894ed7d818a47d6000e132a1074
lewiswolf <lewiswolstanholme@gmail.com>
2020-11-10 23:17:17 +0000

    fixed viewport and src folder

---
+++ web-app/src/aug-real.js
@@ -0,0 +1,27 @@
+// Vidhandler toggles visibility of live-streamed video
+AFRAME.registerComponent('vidhandler', {
+    init: function () {
+        this.toggle = false
+        this.vid = document.querySelector('video')
+    },
+    tick: function () {
+        if (this.el.object3D.visible === true) {
+            if (!this.toggle) {
+                this.toggle = true
+                this.vid.style.visibility = 'visible'
+                // this.vid.play();
+            }
+        } else {
+            if (this.toggle) {
+                this.toggle = false
+                // this.vid.pause();
+                this.vid.style.visibility = 'hidden'
+            }
+        }
+    },
+})
+
+window.onload = () => {
+    // Hide livestream video initially
+    document.querySelector('video').style.visibility = 'hidden'
+}
