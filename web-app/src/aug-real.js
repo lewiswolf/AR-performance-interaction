255be26284f8db713f5e1312b219b0284ee51ca2
lewiswolf <lewiswolstanholme@gmail.com>
2020-11-11 14:12:23 +0000

    deleted error code and added object conditionals

--- web-app/src/aug-real.js
+++ web-app/src/aug-real.js
@@ -1,8 +1,6 @@
 window.onload = () => {
     // Hide livestream video initially
     document.querySelector('video').style.visibility = 'hidden'
-    document.getElementsByTagName(html).style.height = '100%'
-    document.getElementsByTagName(html).style.width = '100%'
 }
 
 // Vidhandler toggles visibility of live-streamed video
@@ -12,7 +10,7 @@ AFRAME.registerComponent('vidhandler', {
         this.vid = document.querySelector('video')
     },
     tick: function () {
-        if (this.el.object3D.visible === true) {
+        if (this?.el?.object3D.visible === true) {
             if (!this.toggle) {
                 this.toggle = true
                 this.vid.style.visibility = 'visible'
