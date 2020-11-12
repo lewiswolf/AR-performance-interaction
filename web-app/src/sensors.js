448db5748f7977b8418cfbcff31020e3aec2c15a
lewiswolf <lewiswolstanholme@gmail.com>
2020-11-12 19:22:42 +0000

    commented out accelorometer

--- web-app/src/sensors.js
+++ web-app/src/sensors.js
@@ -92,86 +92,86 @@ const addGyroListener = (() => {
 })()
 
 
-/* 
+/*
     ROTATIOM
-*/
+// */
 
-let deviceMotion = {
-    x: null,
-    y: null,
-    z: null,
-}
+// let deviceMotion = {
+//     x: null,
+//     y: null,
+//     z: null,
+// }
 
-const getDeviceMotion = (e) => {
-    deviceMotion = {
-        x: e.accelerationIncludingGravity.x,
-        y: e.accelerationIncludingGravity.y,
-        z: e.accelerationIncludingGravity.z,
-    }
-}
+// const getDeviceMotion = (e) => {
+//     deviceMotion = {
+//         x: e.accelerationIncludingGravity.x,
+//         y: e.accelerationIncludingGravity.y,
+//         z: e.accelerationIncludingGravity.z,
+//     }
+// }
 
 
-// button constructor
-const addAccelListener = (() => {
-    let element = document.getElementById('accelButton')
-    let timer // empty setInterval
-    let permissionState = null // permissions for listener
+// // button constructor
+// const addAccelListener = (() => {
+//     let element = document.getElementById('accelButton')
+//     let timer // empty setInterval
+//     let permissionState = null // permissions for listener
 
-    const touchWrapper = (e) => {
-        if (e.cancelable) {
-            e.preventDefault()
-            downListener()
-        }
-    }
+//     const touchWrapper = (e) => {
+//         if (e.cancelable) {
+//             e.preventDefault()
+//             downListener()
+//         }
+//     }
 
-    const downListener = async () => {
-        setStyles(true)
-        // add mouseup listeners
-        element.addEventListener('touchend', upListener)
-        element.addEventListener('touchcancel', upListener)
-        window.addEventListener('mouseup', upListener)
+//     const downListener = async () => {
+//         setStyles(true)
+//         // add mouseup listeners
+//         element.addEventListener('touchend', upListener)
+//         element.addEventListener('touchcancel', upListener)
+//         window.addEventListener('mouseup', upListener)
 
-        // if !permission, request it!
-        if (!permissionState && typeof DeviceMotionEvent.requestPermission === 'function') {
-            permissionState = await DeviceMotionEvent.requestPermission()
-        }
+//         // if !permission, request it!
+//         if (!permissionState && typeof DeviceMotionEvent.requestPermission === 'function') {
+//             permissionState = await DeviceMotionEvent.requestPermission()
+//         }
 
-        if (permissionState) {
-            const post2server = () => {
-                axios.post('/', Object.assign(userID, deviceMotion))
-            }
-            // add listener and start sending
-            window.addEventListener('devicemotion', getDeviceMotion)
-            timer = setInterval(post2server, pollingRate)
-        }
-    }
+//         if (permissionState) {
+//             const post2server = () => {
+//                 axios.post('/', Object.assign(userID, deviceMotion))
+//             }
+//             // add listener and start sending
+//             window.addEventListener('devicemotion', getDeviceMotion)
+//             timer = setInterval(post2server, pollingRate)
+//         }
+//     }
 
-    const upListener = () => {
-        // stop timer and remove listener
-        if (permissionState) {
-            clearInterval(timer)
-            removeEventListener('devicemotion', getDeviceMotion)
-        }
+//     const upListener = () => {
+//         // stop timer and remove listener
+//         if (permissionState) {
+//             clearInterval(timer)
+//             removeEventListener('devicemotion', getDeviceMotion)
+//         }
 
-        setStyles(false)
+//         setStyles(false)
 
-        // remove mouseup listeners
-        element.removeEventListener('touchend', upListener)
-        element.removeEventListener('touchcancel', upListener)
-        window.removeEventListener('mouseup', upListener)
-    }
+//         // remove mouseup listeners
+//         element.removeEventListener('touchend', upListener)
+//         element.removeEventListener('touchcancel', upListener)
+//         window.removeEventListener('mouseup', upListener)
+//     }
 
-    const setStyles = (bool) => {
-        element.innerText = `Accelerometer ${bool ? 'Listening' : 'Off'}`
-        Object.assign(element.style, {
-            background: bool ? '#9cff57' : '#ff7961'
-        })
-    }
+//     const setStyles = (bool) => {
+//         element.innerText = `Accelerometer ${bool ? 'Listening' : 'Off'}`
+//         Object.assign(element.style, {
+//             background: bool ? '#9cff57' : '#ff7961'
+//         })
+//     }
 
-    // add default listeners
-    element.addEventListener('touchstart', touchWrapper)
-    element.addEventListener('touchmove', (e) => e.preventDefault())
-    element.addEventListener('mousedown', downListener)
-    // set default text
-    setStyles(false)
-})()
+//     // add default listeners
+//     element.addEventListener('touchstart', touchWrapper)
+//     element.addEventListener('touchmove', (e) => e.preventDefault())
+//     element.addEventListener('mousedown', downListener)
+//     // set default text
+//     setStyles(false)
+// })()
