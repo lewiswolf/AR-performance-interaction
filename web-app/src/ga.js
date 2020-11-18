7e10b956997100d7c3482c416cf2ea7c1b238ca6
lewiswolf <lewiswolstanholme@gmail.com>
2020-11-18 20:06:51 +0000

    cleanup

---
+++ web-app/src/ga.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || []
+function gtag() {
+    dataLayer.push(arguments)
+}
+gtag('js', new Date())
+
+gtag('config', 'UA-171437116-3')
+gtag('config', 'G-RRYDW48QPH')
